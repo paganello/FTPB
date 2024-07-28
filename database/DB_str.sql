e010af5ef41019f6e2f1d0f5251544accd2e2ee3
paganello <thomas.paganelli03@gmail.com>
2024-07-28 18:44:34 +0200

    added DB schema

---
+++ database/DB_str.sql
@@ -0,0 +1,26 @@
+CREATE TABLE transaction (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    date CHAR(16) NULL,
+    total FLOAT NULL,
+    receipt_ID CHAR(9) NULL,
+    receipt_file_name CHAR(23) NULL
+);
+
+CREATE TABLE good (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_ID INT,
+    amount FLOAT NULL,
+    tax INT NULL,
+    description CHAR(64) NULL,
+    FOREIGN KEY (transaction_ID) REFERENCES transaction(ID)
+);
+
+CREATE TABLE store (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_ID INT,
+    name CHAR(64) NULL,
+    address CHAR(64) NULL,
+    city CHAR(64) NULL,
+    VAT CHAR(16) NULL,
+    FOREIGN KEY (transaction_ID) REFERENCES transaction(ID)
+);
